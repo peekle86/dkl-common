73aa1d5e2db06dbb68547c36e55203a7dac3161a
Misha Beskin <kosherdev@gmail.com>
2021-05-25 13:45:01 +0300

    added constant contact skype

--- src/Constants/SettingsTypes.php
+++ src/Constants/SettingsTypes.php
@@ -8,10 +8,11 @@ class SettingsTypes
 {
     const LANGUAGE_DEFAULT = 'language_default';
     const CONTACT_PHONES = 'contact_phones';
-    const CONTACT_ADDRESS = 'contact_addres';
+    const CONTACT_ADDRESS = 'contact_address';
     const CONTACT_EMAIL = 'contact_email';
     const CONTACT_FACEBOOK = 'contact_facebook';
     const CONTACT_INSTAGRAM = 'contact_instagram';
     const CONTACT_LINKEDIN = 'contact_linkedin';
+    const CONTACT_SKYPE = 'contact_skype';
     const ABOUT_US = 'about_us';
 }
