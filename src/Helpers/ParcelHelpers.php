b8286ab41ea23924ab9103e44610392988c42d05
Misha Beskin <kosherdev@gmail.com>
2021-07-14 12:38:52 +0300

    added parcel helper

--- src/Helpers/ParcelHelpers.php
+++ src/Helpers/ParcelHelpers.php
@@ -10,11 +10,14 @@ class ParcelHelpers
         $divider = in_array($deliveryType, DeliveryTypes::AIR_DELIVERY_TYPES) ? 6000 : 5000;
         return $places->sum(
             function ($place) use ($deliveryType, $divider) {
+                $x=$place['x']??0;
+                $y=$place['y']??0;
+                $z=$place['z']??0;
                 $totalWeight = 0;
                 if ($deliveryType == DeliveryTypes::ROAD_EXPRESS) {
-                    $totalWeight += ($place->x / 100) * ($place->y / 100) * ($place->z / 100) * 333 * $place->quantity;
+                    $totalWeight += ($x / 100) * ($y / 100) * ($z / 100) * 333 * $place['quantity'];
                 } else {
-                    $totalWeight = ($place->x * $place->y * $place->z / $divider) * $place->quantity;
+                    $totalWeight = ($x * $y * $z / $divider) * $place['quantity'];
                 }
                 return number_format($totalWeight, 2);
             }
