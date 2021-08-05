a999aa977b4c8da6baa93ed93f7410ad0517be7c
Misha Beskin <kosherdev@gmail.com>
2021-08-05 12:18:24 +0300

    added hawb helper

---
+++ src/Helpers/HawbHelper.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Helpers;
+
+use DKL\Models\Carrier;
+use DKL\Models\Domain;
+use DKL\Models\Hawb;
+use Carbon\Carbon;
+
+class HawbHelper
+{
+    public static function findNextHawbNumber($domainId)
+    {
+        $domain = Domain::find($domainId);
+        $prefix = $domain->hawb_prefix;
+        $today_date = Carbon::now()->format('ym');
+        $hawbNum = $today_date . '00500';
+
+        $carrier = Carrier::whereCode('dkl')->first();
+        $hawb = Hawb::whereDomainId($domainId)
+            ->whereCarrierId($carrier->id)
+            ->latest()
+            ->first();
+        if ($hawb) {
+            if (preg_match("/^$prefix.*/", $hawb->number)) {
+                $int = preg_replace("/^$prefix(.*)/", "$1", $hawb->number);
+            }
+            $hawbNum = preg_replace('/^\w{0,4}/', $today_date, $int);
+            $hawbNum++;
+        }
+
+        return $prefix . $hawbNum;
+    }
+}
