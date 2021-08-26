4b612df917082dfff57674fefd8bcd8bbab6c7fd
Misha Beskin <kosherdev@gmail.com>
2021-08-26 11:36:30 +0300

    added user address presets

--- src/Models/UserAddressPreset.php
+++ src/Models/UserAddressPreset.php
@@ -7,7 +7,7 @@ namespace DKL\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class UserAddressPresets extends Model
+class UserAddressPreset extends Model
 {
     public function address(): HasOne
     {
