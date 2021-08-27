04d3fdd77852e49bbd2c1640ad8c255f75adc7f2
Misha Beskin <kosherdev@gmail.com>
2021-08-27 11:44:58 +0300

    added user address presets filter

--- src/Models/UserAddressPreset.php
+++ src/Models/UserAddressPreset.php
@@ -4,6 +4,7 @@
 namespace DKL\Models;
 
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -13,5 +14,29 @@ class UserAddressPreset extends Model
     public function address(): BelongsTo
     {
         return $this->belongsTo(Address::class);
+    }
+
+    public static function filter($filter = [], $domainId = 0)
+    {
+        $preset = self::any()->where('domain_id', $domainId);
+        if (isset($filter['nickname'])) {
+            $key = $filter['nickname'];
+            $preset = $preset->where('nickname', 'like', '%' . $key . '%');
+        }
+
+        foreach (['name', 'address', 'city', 'country', 'type'] as $key) {
+            if (array_key_exists($key, $filter)) {
+                $keyFiltered = $filter[$key];
+                $preset = $preset->whereHas(
+                    'address',
+                    function (Builder $query) use ($keyFiltered) {
+                        $query->where('name', 'like', '%' . $keyFiltered . '%');
+                    }
+                );
+            }
+        }
+
+
+        return $preset;
     }
 }
