9a3556a4344ae375dce09ed64e94a807e8502bc8
Misha Beskin <kosherdev@gmail.com>
2021-08-30 10:24:14 +0300

    fixed Parcel helper

--- src/Helpers/ParcelHelpers.php
+++ src/Helpers/ParcelHelpers.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace DKL\Helpers;
+
 use DKL\Constants\DeliveryTypes;
 
 class ParcelHelpers
@@ -10,14 +11,15 @@ class ParcelHelpers
         $divider = in_array($deliveryType, DeliveryTypes::AIR_DELIVERY_TYPES) ? 6000 : 5000;
         return $places->sum(
             function ($place) use ($deliveryType, $divider) {
-                $x=$place['x']??0;
-                $y=$place['y']??0;
-                $z=$place['z']??0;
+                $quantity = $place['quantity'] ?? 0;
+                $x = $place['x'] ?? 0;
+                $y = $place['y'] ?? 0;
+                $z = $place['z'] ?? 0;
                 $totalWeight = 0;
                 if ($deliveryType == DeliveryTypes::ROAD_EXPRESS) {
-                    $totalWeight += ($x / 100) * ($y / 100) * ($z / 100) * 333 * $place['quantity'];
+                    $totalWeight += ($x / 100) * ($y / 100) * ($z / 100) * 333 * $quantity;
                 } else {
-                    $totalWeight = ($x * $y * $z / $divider) * $place['quantity'];
+                    $totalWeight = ($x * $y * $z / $divider) * $quantity;
                 }
                 return number_format($totalWeight, 2);
             }
