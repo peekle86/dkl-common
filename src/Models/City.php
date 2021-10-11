0791cd4c2ca20072c48cac3ca3c38d888bd49920
Misha Beskin <kosherdev@gmail.com>
2021-10-11 06:45:50 +0300

    geo models

---
+++ src/Models/City.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DKL\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class City extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'region_id',
+        'country_id',
+    ];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function region(): BelongsTo
+    {
+        return $this->belongsTo(Region::class);
+    }
+}
